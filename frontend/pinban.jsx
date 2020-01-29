efb30ccb77993c9be165e4134bd8e74e437947e5
max821023 <max821023@gmail.com>
2020-01-28 16:14:47 -0800

    Add login form container

--- frontend/pinban.jsx
+++ frontend/pinban.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
 
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
 
   ReactDOM.render(<Root store={store}/>, root);
 });

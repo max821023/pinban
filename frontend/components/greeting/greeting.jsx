8498d1f633505ad215776701ae57b3efd489707d
max821023 <max821023@gmail.com>
2020-01-31 10:32:06 -0800

    Add home icon and dropdown

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Greeting extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { showDropDown: false }
+  }
 
   render() {
     const { currentUser, logout } = this.props
+    console.log(this.state.showDropDown);
     return (
-    <hgroup className = "header-group" >
+    <div className = "header-group" >
       <div className="navbar">
+        <div className="left-side">
+          <i className="material-icons">home</i>
+        </div>
         <div className="center">
           <i className="fab fa-trello"></i>
           <span className="icon">Pinban</span>
         </div>
+        <div className="right-side">
+          <p className="initials" onClick={() => this.setState({ showDropDown: !this.state.showDropDown})}>ML</p>
+            {this.state.showDropDown && <div>drop down content</div>}
+        </div>
       </div>
       <h2 className="header-name">Hi, {currentUser.username}!</h2>
       <button className="header-button" onClick={logout}>Log Out</button>
-    </hgroup >
+    </div >
     );
   }
 };

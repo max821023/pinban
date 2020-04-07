d435af4edfcca079a2b89e07df4ef6fb135aa41d
max821023 <max821023@gmail.com>
2020-04-07 15:01:33 -0700

    Add footer links

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -22,11 +22,39 @@ const Splash = () => (
     <div className="center-items">
       <div className="left-center">
         <h1>Pinban lets you work more collaboratively and get more done.</h1>
-        <p>Pinban’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.</p>
+        <p>
+          Pinban’s boards, lists, and cards enable you to organize and
+          prioritize your projects in a fun, flexible, and rewarding way.
+        </p>
       </div>
       <div className="right-center">
         <img src={window.pinboard} />
       </div>
+    </div>
+    <div className="footer">
+      <div className="personal-links">
+        <a href="https://github.com/max821023">
+          <button>
+            <i class="fab fa-github"></i>
+          </button>
+        </a>
+        <a href="https://www.linkedin.com/in/max-lin-dev/">
+          <button>
+            <i class="fab fa-linkedin-in"></i>
+          </button>
+        </a>
+        <a href="https://angel.co/max-lin-dev">
+          <button>
+            <i class="fab fa-angellist"></i>
+          </button>
+        </a>
+        <a href="http://www.maxlin-dev.com/">
+          <button>
+            <i class="fas fa-archive"></i>
+          </button>
+        </a>
+      </div>
+      <div className="copyright"></div>
     </div>
   </div>
 );

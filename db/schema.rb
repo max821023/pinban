c82399031b4aa8a018f6dc5fb496e656aec5ce9e
max821023 <max821023@gmail.com>
2020-04-09 11:08:10 -0700

    Update nokogiri and add color column to boards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_175115) do
+ActiveRecord::Schema.define(version: 2020_04_09_180325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_175115) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "archived", default: false
+    t.string "board_background"
     t.index ["creator_id"], name: "index_boards_on_creator_id"
   end
 

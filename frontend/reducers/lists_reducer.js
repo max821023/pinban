48bccb202916051d7b54422678d3adb25ae70ce1
max821023 <max821023@gmail.com>
2020-04-10 14:11:50 -0700

    Add list reducer

---
+++ frontend/reducers/lists_reducer.js
@@ -0,0 +1,16 @@
+import {
+  RECEIVE_LIST
+} from '../actions/list_actions'
+
+const listsReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_LIST:
+      return Object.assign({}, state, action.list)
+    default:
+      return state;
+  }
+};
+
+export default listsReducer;

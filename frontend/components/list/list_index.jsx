67a7b15f15915e6a97a40945e80cb14b4eb5c4e5
max821023 <max821023@gmail.com>
2020-04-11 01:34:44 -0700

    Add list container css

--- frontend/components/list/list_index.jsx
+++ frontend/components/list/list_index.jsx
@@ -5,9 +5,12 @@ class ListIndex extends React.Component {
     super(props)
     this.state = {
       title: "",
-      lists: {}
+      lists: {},
+      addListForm: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.showListForm = this.showListForm.bind(this);
+    this.closeListForm = this.closeListForm.bind(this);
   }
 
   componentDidMount() {
@@ -19,6 +22,7 @@ class ListIndex extends React.Component {
   }
 
   handleSubmit(e) {
+    debugger
     e.preventDefault();
     this.props.createList({
       title: this.state.title,
@@ -27,35 +31,73 @@ class ListIndex extends React.Component {
     })
   }
 
+  showListForm() {
+    this.setState({ addListForm: true })
+  }
+
+  closeListForm(e) {
+    if (this.state.addListForm && 
+      e.currentTarget.className !== "add-list-form-wrapper" && 
+      e.target.className !== "add-list" &&
+      e.target.className !== "fa-times" &&
+      e.target.className !== "list-title-input" &&
+      e.target.className !== "list-add-buttons") {
+      this.setState({ addListForm: false })
+    }
+  }
+
   render() {
-    console.log(this.props.lists)
     
     return (
       <div>
-        <div className="lists-container">
+        <div className="lists-container" onClick={this.closeListForm}>
           <div className="created-lists">
-            {this.props.lists.filter(list => list.archived === false).map(list => (
-              <div className="list-column" key={list.id}>
-                <div className="list-title">{list.title}</div>
-                <div className="card-container">
-                  CARDS!
+            {this.props.lists
+              .filter((list) => list.archived === false)
+              .map((list) => (
+                <div className="list-column" key={list.id}>
+                  <div className="list-content">
+                    <div className="list-title">
+                      {list.title}
+                      <p>
+                        <i class="fas fa-trash-alt"></i>
+                      </p>
+                    </div>
+                    <div className="card-container">CARDS!</div>
+                    <div>
+                      <button>+ Add another card</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
           <div className="add-list">
-            <button className="add-list-button">+ Add a list</button>
-            <form className="add-list-form" onSubmit={this.handleSubmit}>
-              <input className="list-title-input" type="text" value={this.state.title} onChange={this.update('title')} placeholder="Enter list title..."/>
-              <section className="list-add-buttons">
-                <input type="submit" value="Add List"/>
-                <button><i className="fas fa-times"></i></button>
-              </section>
-            </form>
+            <button className="add-list-button" onClick={this.showListForm}>
+              + Add a list
+            </button>
+            {this.state.addListForm ? (
+              <div className="add-list-form-wrapper">
+                <form className="add-list-form" onSubmit={this.handleSubmit}>
+                  <input
+                    className="list-title-input"
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.update("title")}
+                    placeholder="Enter list title..."
+                  />
+                  <section className="list-add-buttons">
+                    <input type="submit" value="Add List" />
+                  </section>
+                </form>
+                <button className="add-list-x" onClick={this.closeListForm}>
+                  <i className="fas fa-times"></i>
+                </button>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
